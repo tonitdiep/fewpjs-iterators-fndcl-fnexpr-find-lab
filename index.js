4e3a258422b2f19154d044ce6dc4f7c2b6e91be3
tonitdiep <adiep123@gmail.com>
2021-04-04 22:32:04 -0400

    commit tree 1e01df7a79dcb76e51d9d1aa17232a3b5ea66560

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 
+
+    function superbowlWin (record) {
+        let result = record.find( record => record.result === "W")
+        return !!result ? result.year : undefined
+    }
+
+
+    // Write a function called superbowlWin() in index.js that: * Receives 1 argument, an Array of JavaScript Objects 
+    // * Each object has two properties: year and result * Use find() to test each Object to see if the result is "W" — a win! 
+    // * superbowlWin() should return the year when the win occurred (if it occurred at all!). If no win is found, it should return, sadly, undefined
+
+
+    // Array.find() is a built-in function in JavaScript
+    // which is used to get the value of the first element
+    // in the array that satisfies the provided condition. 
+    // With this, you can quickly check all the elements of 
+    // the array and returns the first match.
+
